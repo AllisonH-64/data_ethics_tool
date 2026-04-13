8096e3f93841d0472d1e3e89cc8ca95d181212a7
AllisonH-64 <allison.jaheim@gmain.com>
2026-04-13 11:12:33 -0400

    created a new file: sample_clean_input.py

---
+++ sample_clean_input.py
@@ -0,0 +1,5 @@
+"""Sample clean input file for testing the data ethics tool."""
+
+value = 2 + 2
+result = value * 3
+print(result)
---
+++ sample_test_input.py
@@ -0,0 +1,5 @@
+"""Sample input file for testing the data ethics tool."""
+
+value = 2 + 2
+result = eval("value * 3")
+print(result)
