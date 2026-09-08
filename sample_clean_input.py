c3843b624c95b70944d72ccef03699a571a26705
AllisonH-64 <allison.jaheim@gmail.com>
2026-09-08 08:46:44 -0400

    deleted five files

--- sample_clean_input.py
+++
@@ -1,5 +0,0 @@
-"""Sample clean input file for testing the data ethics tool."""
-
-value = 2 + 2
-result = value * 3
-print(result)
--- sample_test_input.py
+++
@@ -1,5 +0,0 @@
-"""Sample input file for testing the data ethics tool."""
-
-value = 2 + 2
-result = eval("value * 3")
-print(result)
